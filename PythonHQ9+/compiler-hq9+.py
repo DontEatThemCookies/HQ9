40ae479cdba92e23b07224ea29a181ca57f3bf9a
David Costell <davidcostell44@gmail.com>
2021-09-30 18:52:54 +0800

    PyHQ9+ Compiler - Beta 2
    Implemented the 99 Bottles of Beer function
    Other optimizations

--- PythonHQ9+/compiler-hq9+.py
+++ PythonHQ9+/compiler-hq9+.py
@@ -20,40 +20,48 @@
 import os # for exception catching
 import sys # for exception catching
 import datetime # to record exec time
+import inspect # for 99 bottles
 from time import sleep # for timed delays
 ###########################
 
 
 ################## DEFINITIONS ##################
 def beersong():    
-        for i in range (99,-1,-1):
-            if i > 0:
+        for i in range (99,-1,-1): # 99 Bottles of Beer            
+            if i > 2:
                 print(i, "bottles of beer on the wall,", i, " bottles of beer,")
                 print("Take one down and pass it around,", i-1, "bottles of beer on the wall.\n")
-                sleep(0.1)
+                
+            elif i == 2:
+                    print(i, "bottles of beer on the wall,", i, " bottles of beer,")
+                    print("Take one down and pass it around,", i-1, "bottle of beer on the wall.\n")
+            elif i == 1:
+                    print(i, "bottle of beer on the wall,", i, " bottle of beer,")
+                    print("Take one down and pass it around,", i-1, "bottles of beer on the wall.\n")
             else:
                 print("No more bottles of beer on the wall, no more bottles of beer.")
                 print("Go to the store and buy some more, 99 bottles of beer on the wall.")
-                sleep(0.5)
                 
 '''Pre-defines the commands to be written to file
-Needed to prevent NameErrors (prints blank if that certain var wasn't called)'''
+Needed to prevent NameErrors (prints blank line if that certain var wasn't called)'''
 h = ""
 h1 = ""
 q = ""
 q1 = ""
 q2 = ""
 nine = ""
+prenine1 = ""
+prenine2 = ""
 p = ""
 p1 = ""
 p2 = ""
 p3 = ""
 linux = "#!/usr/bin/env python3 \n"
-header = "# HQ9+ compiled into Python 3 - "+str(datetime.datetime.now())
+header = "#Compiled by the PyHQ9+ Compiler (Python 3) - "+str(datetime.datetime.now())
 n = " \n" # Newline
 
 acm = 0 # Accumulator
-acm2 = "acm = 0 # Accumulator"
+acm2 = "acm = 0 # Accumulator"+n
 
 ##################################################
 
@@ -66,7 +74,7 @@ print("")
 name = input("Name the file (e.g. myhq9program): ")
 print("")
 
-A = input("Input commands [valid: H, Q, +]: ")
+A = input("Input commands [valid: H, Q, 9, +]: ")
 begin = datetime.datetime.now()
 print("BEGIN COMPILE")
 print("#######################################")
@@ -80,29 +88,29 @@ try:
         filename = name+".py"
         with open(filename, 'x') as f:
                 
-
                 if 'H' in A: # Hello, World!
                         h1 = "H = A.count('H') # Hello World"
-                        h = "print(H*'Hello, World! ')"
+                        h = "print(H*'Hello, World! ') \n"
 
                 if 'Q' in A: # The quine function
                         q1 = "q1 = A.count('Q') # Quine function"
                         q2 = "q2 = q1 * q1"
-                        q = "print(q2*'Q')"
+                        q = "print(q2*'Q') \n"
                     
-                '''if '9' in A: # 99 Bottles of Beer - WIP
-                        nine = ""+str(beersong())+""'''
+                if '9' in A: # 99 Bottles of Beer
+                        prenine1 = inspect.getsource(beersong)
+                        prenine2 = "beersong()"+n
+                        nine = prenine1+prenine2+n
                 
                 if '+' in A: # Accumulator
                         p = "acmc = A.count('+') # Accumulator"
                         p1 = "for _ in range(acmc):"
                         p2 = "    acm = acm + 1"
-                        p3 = "    print('Accumulator has been incremented!')"
+                        p3 = "    print('Accumulator has been incremented!') \n"
 
                 writeoutput = linux+header+n+A1+n+acm2+n+h1+n+h+n+q1+n+q2+n+q+n+nine+p+n+p1+n+p2+n+p3+n+fnl
                 # Spaghetti code - will be optimized soon
-                # To do: add "+n" after "+nine" once beersong func is made
-                                               
+                
                 f.write(writeoutput)
                 
              
@@ -110,7 +118,7 @@ try:
 except FileExistsError:
         print("An error occurred while attempting compile!")
         print("Error: File already exists with the same name -", sys.exc_info()[0])
-        e = input("Would you like to remove the file? [Y/N] ")
+        e = input("Would you like to remove the file? (Irreversible!) [Y/N] ")
         if e == "Y":
                 os.remove(filename)
                 print("File deleted.")
@@ -132,7 +140,6 @@ except: # for other errors
         os.remove(filename)
         sleep(5)
 
-
 print("END COMPILE")
 print("The code has compiled successfully.")
 print("File compiled as: "+filename)
@@ -141,4 +148,4 @@ endtime = datetime.datetime.now() - begin
 print(endtime)
 sleep(3)
 
-# Beta 1
+# Beta 2
